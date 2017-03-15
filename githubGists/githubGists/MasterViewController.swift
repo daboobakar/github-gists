d6238d0a5acd2a02390be3bc75b40fe6308a9c78
Danyal Aboobakar <danyal.aboobakar@gmail.com>
2017-03-15 18:39:17 +0000

    Optimized app by caching user's avatar images

--- githubGists/githubGists/MasterViewController.swift
+++ githubGists/githubGists/MasterViewController.swift
@@ -12,13 +12,15 @@ class MasterViewController: UITableViewController {
     
     var detailViewController: DetailViewController? = nil
     var gists = [Gist]()
-
-
+    var imageCache = [String: UIImage?]()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.navigationItem.leftBarButtonItem = self.editButtonItem
-
+        
         let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(insertNewObject(_:)))
         self.navigationItem.rightBarButtonItem = addButton
         if let split = self.splitViewController {
@@ -43,7 +45,6 @@ class MasterViewController: UITableViewController {
             }
             self.tableView.reloadData()
         }
-
         
         self.tableView.reloadData()
     }
@@ -51,29 +52,25 @@ class MasterViewController: UITableViewController {
     func handleLoadGistsError(_ error: Error) {
         // TODO: show error
     }
-
+    
     override func viewWillAppear(_ animated: Bool) {
         self.clearsSelectionOnViewWillAppear = self.splitViewController!.isCollapsed
         super.viewWillAppear(animated)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     func insertNewObject(_ sender: Any) {
-        let alert = UIAlertController(title: "Not Implemented",
-                                      message: "Can't create new gists yet, will implement later",
-                                      preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "OK",
-                                      style: .default,
-                                      handler: nil))
+        let alert = UIAlertController(title: "Not Implemented", message: "Can't create new gists yet, will implement later", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
         self.present(alert, animated: true, completion: nil)
     }
-
+    
     // MARK: - Segues
-
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showDetail" {
             if let indexPath = self.tableView.indexPathForSelectedRow {
@@ -85,20 +82,20 @@ class MasterViewController: UITableViewController {
             }
         }
     }
-
+    
     // MARK: - Table View
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return gists.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-
+        
         let gist = gists[indexPath.row]
         cell.textLabel!.text = gist.description
         cell.detailTextLabel?.text = gist.ownerLogin
@@ -107,32 +104,40 @@ class MasterViewController: UITableViewController {
         cell.imageView?.image = nil
         //check we have urlString for image
         if let urlString = gist.ownerAvatarURL {
-            GithubAPIManager.sharedInstance.imageFrom(urlString: urlString) {
-                //check if errors exist, if so print error
-                (image, error) in
-                guard error == nil else {
-                    print(error!)
-                    return
+            if let cachedImage = imageCache[urlString] {
+                cell.imageView?.image = cachedImage
+            } else {
+                GithubAPIManager.sharedInstance.imageFrom(urlString: urlString) {
+                    //check if errors exist, if so print error
+                    (image, error) in
+                    guard error == nil else {
+                        print(error!)
+                        return
+                    }
+                    
+                    //Save the image so we won't have to keep fetching it when user scrolls
+                    self.imageCache[urlString] = image
+                    
+                    //set cell's image if no error exists
+                    if let cellToUpdate = self.tableView?.cellForRow(at: indexPath) {
+                        cellToUpdate.imageView?.image = image // will work fine even if image is nil
+                        //need to reload the view, which won't happen otherwise
+                        // since this is in an async call
+                        cellToUpdate.setNeedsLayout()
+                    }
                 }
-                //set cell's image if no error exists
-                if let cellToUpdate = self.tableView?.cellForRow(at: indexPath) {
-                    cellToUpdate.imageView?.image = image // will work fine even if image is nil
-                    //need to reload the view, which won't happen otherwise
-                    // since this is in an async call
-                    cellToUpdate.setNeedsLayout()
-                }
+                
             }
+            
         }
         return cell
     }
-
+    
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return false
     }
     
-    
-
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             gists.remove(at: indexPath.row)
@@ -141,7 +146,7 @@ class MasterViewController: UITableViewController {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
         }
     }
-
-
+    
+    
 }
 
